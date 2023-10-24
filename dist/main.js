cda857597a4f369aa1154dd5e4ddcd4f22f080ed
Lucas <luke@nethercott.com>
2023-10-24 17:55:19 -0600

    another attempt at the spawning problem

--- dist/main.js
+++ dist/main.js
@@ -6,18 +6,24 @@ const creeperClasses = require('creeperClasses')
 module.exports.loop = function () {
   // spawn new creeps
   if (Game.spawns['Spawn1'].store[RESOURCE_ENERGY] >= 300) {
-    if (creeperClasses[0].currentCount < creeperClasses[0].maxCount) {
-      Game.spawns['Spawn1'].spawnCreep(creeperClasses[0].parts, `${creeperClasses[0].role}${Game.time.toString()}`, {
-        memory: creeperClasses[0].memory,
-      })
-    } else if (creeperClasses[1].currentCount < creeperClasses[1].maxCount) {
-      Game.spawns['Spawn1'].spawnCreep(creeperClasses[1].parts, `${creeperClasses[1].role}${Game.time.toString()}`, {
-        memory: creeperClasses[1].memory,
-      })
-    } else if (creeperClasses[2].currentCount < creeperClasses[2].maxCount) {
-      Game.spawns['Spawn1'].spawnCreep(creeperClasses[2].parts, `${creeperClasses[2].role}${Game.time.toString()}`, {
-        memory: creeperClasses[2].memory,
-      })
+    const spawnOdds = Math.random()
+    let spawnIndex
+    if (spawnOdds > 0.6) {
+      spawnIndex = 0
+    } else if (spawnOdds > 0.3) {
+      spawnIndex = 1
+    } else {
+      spawnIndex = 2
+    }
+
+    if (creeperClasses[spawnIndex].currentCount < creeperClasses[spawnIndex].maxCount) {
+      Game.spawns['Spawn1'].spawnCreep(
+        creeperClasses[spawnIndex].parts,
+        `${creeperClasses[spawnIndex].role}${Game.time.toString()}`,
+        {
+          memory: creeperClasses[spawnIndex].memory,
+        }
+      )
     }
   }
 
