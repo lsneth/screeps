f797378b3abad1d5921b8cb4ed778da33524264b
Luke Nethercott <luke+github@nethercott.com>
2023-10-24 12:29:36 -0600

    initial setup with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-screeps')
+
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: process.env.EMAIL,
+        token: process.env.AUTH_TOKEN,
+        branch: 'default',
+        //server: 'season'
+      },
+      dist: {
+        src: ['dist/*.js'],
+      },
+    },
+  })
+}
