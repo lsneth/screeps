633035373c6a425f019ee7bbd3e17f2e2cd74848
Luke Nethercott <luke+github@nethercott.com>
2023-10-31 22:59:49 -0600

    max harvesters = empty spaces by sources

--- dist/main.spawn.js
+++ dist/main.spawn.js
@@ -4,6 +4,32 @@ const { workerRoles, workerSpawnPool } = require('./role.roles')
 function mainSpawn() {
   // define spawn structure to spawn at
   const spawn = Game.spawns.Spawn1
+
+  // if max harvester count isn't already saved to room memory
+  if (!spawn.room.memory.maxRoleCounts) {
+    spawn.room.memory.maxRoleCounts = {} // TODO: make this section build itself from the very start
+    spawn.room.memory.maxRoleCounts.harvester = 0
+
+    // map through all sources
+    spawn.room.find(FIND_SOURCES).map((source) => {
+      // get 3x3 grid of squares around source
+      const area = spawn.room.lookAtArea(source.pos.y - 1, source.pos.x - 1, source.pos.y + 1, source.pos.x + 1)
+      // count how many of them are wall terrain
+      let count = 0
+      for (const y in area) {
+        for (const x in area[y]) {
+          area[y][x].map((element) => {
+            if (element.type === 'terrain' && element.terrain === 'wall') {
+              count += 1
+            }
+          })
+        }
+      }
+      // add to current maxHarvesterCount: 9 total squares minus the wall count to get the available count
+      spawn.room.memory.maxRoleCounts.harvester += 9 - count
+    })
+  }
+
   // if the spawn isn't busy spawning
   if (!spawn.spawning) {
     // if there isn't already a creep role queued
@@ -31,21 +57,26 @@ function mainSpawn() {
     // attempt to spawn the creep role in memory
     const role = workerRoles[spawn.memory.nextRole]
     const noHarvesters = _.sum(Game.creeps, (creep) => creep.memory.role === 'harvester') === 0 // in case all harvesters are dead
-    // TODO: figure out a better way to do the 300 thing. It's here to counteract the -300 in utils.makeCreeperBody
     const energy = noHarvesters ? spawn.room.energyAvailable : spawn.room.energyCapacityAvailable
     const body = makeCreeperBody(energy, spawn.memory.nextRole)
 
     const spawnResult = spawn.spawnCreep(body, `${role.name}${Game.time.toString()}`, {
       memory: role.memory,
     })
-    if (spawnResult === OK) {
-      // set nextRole to undefined so it picks a new role next time
-      spawn.memory.nextRole = undefined
-      console.log('Spawn Success: ', role.name)
-    } else if (spawnResult === ERR_NOT_ENOUGH_ENERGY) {
-      // console.log('not enough energy')
-    } else {
-      console.log('Spawn Fail: ', spawnResult)
+
+    switch (spawnResult) {
+      case OK:
+        // set nextRole to undefined so it picks a new role next time
+        spawn.memory.nextRole = undefined
+        console.log('Spawn Success: ', role.name)
+        break
+      case ERR_NOT_ENOUGH_ENERGY:
+        // console.log('not enough energy')
+        break
+
+      default:
+        console.log('Spawn Fail: ', spawnResult)
+        break
     }
   }
 }
