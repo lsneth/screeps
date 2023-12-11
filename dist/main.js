2e6e0f0410ed0f6a4bbf6707dba0ddfd6b21067d
Luke Nethercott <luke+github@nethercott.com>
2023-12-11 15:32:41 -0700

    fresh start

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,1 @@
-const mainSpawn = require('./main.spawn')
-const mainActions = require('./main.actions')
-const mainCleanup = require('./main.cleanup')
-
-module.exports.loop = function () {
-  // spawn creep(s)
-  mainSpawn()
-
-  // creep actions
-  mainActions()
-
-  // cleanup function
-  mainCleanup()
-}
+module.exports.loop = function () {}
